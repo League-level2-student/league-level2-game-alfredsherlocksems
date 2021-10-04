b1572987a3a07f525b3fbbcc4be2a4df48ce35e0
student <student@ip-172-31-48-50.us-west-2.compute.internal>
2021-10-04 16:55:57 -0700

    Started chess game.

--- src/chess/ChessRunner.java
+++ src/chess/ChessRunner.java
@@ -1,7 +1,25 @@
 package chess;
 
+import javax.swing.JFrame;
+
 public class ChessRunner {
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
+	GamePanel panel;
+	
+	public ChessRunner() {
+		frame = new JFrame();
+		panel = new GamePanel();
+	}
 	public static void main (String [] args) {
-		System.out.println("Monkeys are cool.");
+		ChessRunner chessRunner = new ChessRunner();
+		chessRunner.setup();
+	}
+	void setup () {
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
--- src/chess/GamePanel.java
+++ src/chess/GamePanel.java
@@ -1,7 +1,88 @@
 package chess;
 
-public class GamePanel {
-	int [][] grid = new int [8][8];
-	//for (int rows = 0; rows < grid.length; rows++) for (int col = 0; cols < grid[rows].length; rows++) } Code for for loops
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener {
+	int [][] grid = {
+			{25, 23, 24, 29, 20, 24, 23, 25},
+			{1, 1, 1, 1, 1, 1, 1, 1},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{11, 11, 11, 11, 11, 11, 11, 11},
+			{15, 13, 14, 19, 10, 14, 13, 15}
+	};
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	Timer drawFrame;	
+	int currentState = GAME;
+	
+	
+	public GamePanel () {
+		drawFrame = new Timer(1000/60, this);
+		drawFrame.start();
+	}
+	public void paintComponent(Graphics g) {
+		if (currentState == MENU) {
+			DrawMenu(g);
+		}
+		else if(currentState == GAME) {
+			DrawGame(g);
+		}
+		else {
+			DrawEnd(g);
+		}
+	}
+	
+	
+	void UpdateMenuState() {
+		
+	}
+	
+	void UpdateGameState() {
+		
+	}
+	
+	void UpdateEndState() {
+		
+	}
+	
+	
+	void DrawMenu(Graphics g) {
+		
+	}
+	void DrawGame(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, ChessRunner.WIDTH, ChessRunner.HEIGHT);
+		for (int rows = 0; rows < grid.length; rows++)  {
+			for (int col = 0; col < grid[rows].length; col++) {
+				if (rows + col % 2 == 0) {
+					g.setColor(Color.white);
+				}
+				else {
+					g.setColor(Color.black);
+				}
+				g.fillRect(col * ChessRunner.WIDTH / 8, rows * ChessRunner.HEIGHT / 8, ChessRunner.WIDTH / 8, ChessRunner.HEIGHT / 8);
+				System.out.println("Columns = " + col + " and rows = " + rows + "." + " The sum of the rows and columns is " + (col + rows) + ".");
+			} 
+		}
+	}
+	void DrawEnd(Graphics g) {	
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
 }
